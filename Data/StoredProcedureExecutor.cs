1d19dd7b9e63b87d8002fd30382f06b451e9d25c
thesilenceofthewolves <waxun@protonmail.com>
2026-07-06 14:56:15 +0100

    new change added to fix errors

--- Data/StoredProcedureExecutor.cs
+++ Data/StoredProcedureExecutor.cs
@@ -1,16 +1,6 @@
-using Microsoft.Data.SqlClient;
-using System.Data;
-
-public class StoredProcedureExecutor
+public async Task<DataTable> Execute(string procedure, Dictionary<string, object> parameters)
 {
-    private readonly SqlConnectionFactory _factory;
-
-    public StoredProcedureExecutor(SqlConnectionFactory factory)
-    {
-        _factory = factory;
-    }
-
-    public async Task<DataTable> Execute(string procedure, Dictionary<string, object> parameters)
+    try
     {
         using var conn = _factory.Create();
         using var cmd = new SqlCommand(procedure, conn)
@@ -19,7 +9,12 @@ public class StoredProcedureExecutor
         };
 
         foreach (var p in parameters)
-            cmd.Parameters.AddWithValue(p.Key, p.Value);
+        {
+            cmd.Parameters.AddWithValue(
+                p.Key,
+                p.Value ?? DBNull.Value
+            );
+        }
 
         await conn.OpenAsync();
 
@@ -28,5 +23,10 @@ public class StoredProcedureExecutor
         dt.Load(reader);
 
         return dt;
+    }
+    catch (SqlException ex)
+    {
+        // 🔥 THIS WILL SHOW REAL SQL ERROR IN AZURE
+        throw new Exception("SQL ERROR: " + ex.Message, ex);
     }
 }
